96d27f684b906740d56c98503c0e8577affe14cb
Vishnu Prasad Gettam <vishnugettam@gmail.com>
2023-01-21 23:07:52 +0000

    final model folder

---
+++ models/final/customer_details.sql
@@ -0,0 +1,23 @@
+{{ config(materialized="view") }}
+
+with
+    customer as (select * from {{ ref("dim_customer") }}),
+    customer_address as (select * from {{ ref("dim_customer_address") }}),
+    customer_demographics as (select * from {{ ref("dim_customer_demographics") }})
+select
+
+    customer.customerid as customer_id,
+    customer.firstname as first_name,
+    customer.lastname as last_name,
+    customer.emailaddress as emailaddress,
+    customer_address.completeaddress as customer_address,
+    customer_demographics.gender,
+    customer_demographics.maritalstatus,
+    customer_demographics.educationstatus
+from customer
+inner join
+    customer_address on customer_address.currentaddress_sk = customer.currentaddress_sk
+inner join
+    customer_demographics
+    on customer.currentdemo_sk = customer_demographics.currentdemo_sk
+;
